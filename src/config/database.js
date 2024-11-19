3309390e9ec2c8d866e50f11da5432248ecdaeaf
shivendra <shivendrakeshari581@gmail.com>
2024-11-20 02:01:37 +0530

    change db connection

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://gk630471:T3k1nvTVn5wHTqCN@cluster0.ek0rw.mongodb.net/DEVTINDER"
+    "mongodb+srv://shivendrakeshari581:ViI9WpS7apqTVfeQ@cluster0.xdw7m.mongodb.net/DEVtinder"
   );
 };
 
